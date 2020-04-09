fde57b50d258747e4870f2647b62d707c400f1ca
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-09 23:00:19 +0530

    Add files via upload

---
+++ effective_parenting.php
@@ -0,0 +1,222 @@
+
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="description" content="Stack admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+  <meta name="keywords" content="admin template, stack admin template, dashboard template, flat admin template, responsive admin template, web app">
+  <meta name="author" content="PIXINVENT">
+  <title>Health Care</title>
+  <link rel="apple-touch-icon" href="maternity/logo.png">
+  <link rel="shortcut icon" type="image/x-icon" href="maternity/logo.png">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
+  rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
+  rel="stylesheet">
+
+  <!-- BEGIN VENDOR CSS-->
+
+  <link rel="stylesheet" type="text/css" href="app-assets/css/vendors.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/forms/icheck/icheck.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/forms/icheck/custom.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/charts/morris.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/extensions/unslider.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/weather-icons/climacons.min.css">
+  <!-- END VENDOR CSS-->
+  <!-- BEGIN STACK CSS-->
+  <link rel="stylesheet" type="text/css" href="app-assets/css/app.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <!-- END STACK CSS-->
+  <!-- BEGIN Page Level CSS-->
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/menu/menu-types/vertical-menu.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/colors/palette-climacon.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/colors/palette-gradient.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/fonts/simple-line-icons/style.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/fonts/meteocons/style.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/pages/users.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- END Page Level CSS-->
+  <!-- BEGIN Custom CSS-->
+  <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <!-- END Custom CSS--><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<style>
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 75%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+.w3-container:hover {
+  opacity: 0.8;
+}
+
+.w3-container:hover .middle {
+  opacity: 1;
+}
+
+
+.text {
+  background-color:black;
+  opacity:0.8;
+  color:white;
+  font-size: 16px;
+  padding: 16px 32px;
+}
+.video{
+  display:block;
+  margin-left: 300px;
+
+}
+.glow {
+  font-size: 40px;
+  color: #0a0a0a;
+  text-align: center;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+  }
+  
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #FFBBA8, 0 0 40px #FFBBA8, 0 0 50px #FFBBA8, 0 0 60px #FFBBA8, 0 0 70px #FFBBA8, 0 0 80px #FFBBA8;
+  }
+}
+</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<nav class="navbar navbar-expand-sm bg-white navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#"></a>
+    <img src="images/logoo.jpg" alt="Bootstrappin" width="70"></a> 
+</head>
+<body>
+
+<h5 class="glow">GiftedMom</h5>
+     
+     
+  <ul class="navbar-nav " style="margin-left: 500px">
+    <li class="nav-item ">
+      <a class="nav-link text-dark " href="first.php"><strong><h4>HOME</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="about.php"><strong><h4>ABOUT</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="nutritional_insights.php"><strong><h4>NUTRITIONAL INSIGHTS</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="effective_parenting.php"><strong><h4>EFFECTIVE PARENTING</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="login.php"><strong><h4>LOGIN</h4></strong></a>
+    </li>
+  </ul>
+</nav>
+
+
+<div class="w3-container bg-light">
+            <img src="images/capture.jpg" alt="Avatar" class="w3-grayscale" style="width:100%">
+            <div class="middle">
+            </div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<h3><p>During the first 10 days home with your baby, some things will come very naturally, others may not. To help you get off to a good start, we created this quick guide for navigating the baby basics.</p>
+<p>During the first 10 days, knowing how to take care of your newborn baby can make these days full of fun and excitement and lead to minimal stress.GiftedMom is here to help you get off to a good start with a easy list of baby care tips for new moms!</p></h3>
+<br>
+<br>
+<h2><strong>HOLDING</strong></h2>
+<br>
+<br>
+<h3><p>Your new-born may feel fragile and delicate to you, but don’t be afraid to touch, handle or hold her! In fact, studies show that babies who are held for more than 2 hours per day thrive better and cry less. No baby care guide can be complete without this valuable advice! Know about the various baby massage benefits.</p></h3>
+<br>
+<h3><p>Remember: Your newborn’s neck muscles are not yet developed, so you will need to support her head whenever you pick her up. You should also support her head against your shoulder or with your opposite hand while carrying her.</p></h3>
+<br>
+<div class="video">
+<video width="700" controls>
+      <source src="videos/video1.mp4" type="video/mp4">
+    </video>
+</div>
+<br>
+<br>
+<h2><strong>BATHING</strong>
+  <br>
+  <br>
+  <h3><p>Baby care after birth invariably involves bathing your baby, which is one of the biggest challenges for a new mom. Learn how to bathe a baby and make sure you have all of the bathing supplies ready before she arrives so you don’t have to miss out on a moment with your new little one. Go for a gentle cleanser formulated for babies. If your baby has more hair, you might try a gentle shampoo . Don’t be afraid to gently wash the soft spot (called fontanels) on your baby's head</p></h3>
+  <br>
+  <div class="video">
+<video width="700" style="position: center" controls>
+      <source src="videos/video2.mp4" type="video/mp4">
+    </video>
+</div>
+<br>
+<br>
+<h2><strong>BABY MASSAGE</strong></h2>
+<br>
+<br>
+<h3><p>When massaging your baby, use a small amount of an appropriately formulated oil, like baby oil —enough for your hands to glide comfortably over your baby’s skin.<p>
+<p>
+Massaging your baby with oil reduces friction, helping make your strokes smoother and more rhythmic. In a study comparing 1-month-old infants massaged with oil versus infants who were not, infants who were massaged with oil showed less fidgeting, less grimacing and less stress behavior (mouthing and clenching fists).</p>
+</p>
+But be careful when choosing an oil to use on your baby’s skin. Natural oils that aren’t specifically formulated for your baby’s skin, like store-bought cooking-grade olive oil, may damage the skin barrier, potentially drying out your baby’s skin. Pure pharmaceutical grade mineral oil, like that used in all of  baby oils, supports the protective function of the skin barrier and helps prevent water loss, relieving symptoms of dry skin while not clogging pores.</p></h3>
+<br>
+<div class="video">
+<video width="700" style="position: center" controls>
+      <source src="videos/video3.mp4" type="video/mp4">
+    </video>
+</div>
+<br>
+<br>
+
+<h2><strong>FRESHENING</strong></h2>
+<br>
+<br>
+<h3><p>Freshening your little one can be a great bonding experience after every bath and diaper change. You can use  Baby Powder, that protects your baby's skin from excess moisture and leaves it soft and smooth. It eliminates friction while keeping skin cool and comfortable. Trust us, it’s a classic!</p>
+<p>
+Wipes
+
+For those times when you’re out and about or when washing hands is simply not convenient, you can still comfortably keep your baby clean and fresh. Baby Skincare Wipes help in keeping your baby's skin clean, while staying soft, supple and healthy.</p></h3>
+<br>
+<div class="video">
+<video width="700" style="position: center" controls>
+      <source src="videos/video4.mp4" type="video/mp4">
+    </video>
+</div>
+</h2>
+</div>
+<br>
+<br>
+<br>
+
+
+
+
+  
+
+  
+
+
+
+
+
+</body>
+</html>
