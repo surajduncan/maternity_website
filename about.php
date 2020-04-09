acdcaf2fe6e77d9407be01e8f3d0b8e6bc046236
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-09 22:59:23 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="description" content="Stack admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+  <meta name="keywords" content="admin template, stack admin template, dashboard template, flat admin template, responsive admin template, web app">
+  <meta name="author" content="PIXINVENT">
+  <title>Health Care</title>
+  <link rel="apple-touch-icon" href="maternity/logo.png">
+  <link rel="shortcut icon" type="image/x-icon" href="maternity/logo.png">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
+  rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
+  rel="stylesheet">
+
+  <!-- BEGIN VENDOR CSS-->
+
+  <link rel="stylesheet" type="text/css" href="app-assets/css/vendors.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/forms/icheck/icheck.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/forms/icheck/custom.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/charts/morris.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/extensions/unslider.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/weather-icons/climacons.min.css">
+  <!-- END VENDOR CSS-->
+  <!-- BEGIN STACK CSS-->
+  <link rel="stylesheet" type="text/css" href="app-assets/css/app.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <!-- END STACK CSS-->
+  <!-- BEGIN Page Level CSS-->
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/menu/menu-types/vertical-menu.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/colors/palette-climacon.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/colors/palette-gradient.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/fonts/simple-line-icons/style.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/fonts/meteocons/style.min.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/pages/users.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- END Page Level CSS-->
+  <!-- BEGIN Custom CSS-->
+  <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <!-- END Custom CSS--><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<style>
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 75%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+.w3-container:hover {
+  opacity: 0.8;
+}
+
+.w3-container:hover .middle {
+  opacity: 1;
+}
+
+
+.text {
+  background-color:black;
+  opacity:0.8;
+  color:white;
+  font-size: 16px;
+  padding: 16px 32px;
+}
+.glow {
+  font-size: 40px;
+  color: #0a0a0a;
+  text-align: center;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+  }
+  
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #FFBBA8, 0 0 40px #FFBBA8, 0 0 50px #FFBBA8, 0 0 60px #FFBBA8, 0 0 70px #FFBBA8, 0 0 80px #FFBBA8;
+  }
+}
+</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<nav class="navbar navbar-expand-sm bg-white navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#"></a>
+    <img src="images/logoo.jpg" alt="Bootstrappin" width="70"></a> 
+</head>
+<body>
+
+<h5 class="glow">GiftedMom</h5>
+
+     
+  <ul class="navbar-nav " style="margin-left: 500px">
+    <li class="nav-item ">
+      <a class="nav-link text-dark " href="first.php"><strong><h4>HOME</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="about.php"><strong><h4>ABOUT</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="nutritional_insights.php"><strong><h4>NUTRITIONAL INSIGHTS</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="effective_parenting.php"><strong><h4>EFFECTIVE PARENTING</h4></strong></a>
+    </li>
+    <li class="nav-item float-right">
+      <a class="nav-link text-dark " href="login.php"><strong><h4>LOGIN</h4></strong></a>
+    </li>
+  </ul>
+</nav>
+<!----end of navbar--->
+<br>
+<br>
+<div class="w3-container"style="left: 30px">
+<div class="jumbotron jumbotron-fluid" color="bg-light" style="left:30px">
+    <div class="col-sm-8" style="left:30px">
+
+      <h2><strong>About Us</strong></h2><br>
+      <br>
+      <h4><p>At GiftedMom, we believe that a child is life’s greatest gift and pregnancy is one of the most magical experiences nature can offer. This gift 
+      deserves to be nurtured and cared for, not just at birth, but for the entire duration of your pregnancy.</p>
+      <p> Which is why, to ensure a healthy and happy pregnancy, this website provides you with world-class medical expertise, state-of-the-art facilities, a space filled with love and laughter and staff dedicated to the holistic well-being of you and your baby.
+      Having celebrated over 45,000 births across regions, with a 3000+ strong team, GiftedMom has expanded its reach to multiple facilities across India while continuing to maintain and excel international standards of care and is still going strong with plans to reach every woman and child in India. Our medical expertise and comprehensive care extends well past maternity services.
+      We are immensely passionate about what we do, and have the support of top-tier investors, Matrix Partners, Sequoia Capital and True North. From a single hospital in South Bangalore, GiftedMom has expanded its operations to ten hospitals spread across Bangalore, Chennai, Gurgaon, Mumbai and Pune. </h4></p>
+    </div>
+     <div class="row">
+    <div class="col-sm-4">
+       <img src="images/img4.jpg" class="img-circle" alt="Cinque Terre" width="304" height="380"> 
+    </div>
+  </div>
+  <div class="middle">
+</div>
+</div>
+</div>
+<br>
+<div class="container text-center">
+  <h3><strong>WE MAKE THE WAITING WONDERFUL!</strong></h3>
+  <br>
+  <br>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <p><strong>blahh</strong></p><br>
+      <img src="images/img5.jpg" alt="Random Name" class="img-thumbnail" width="255" height="240">
+    </div>
+    <div class="col-sm-4">
+      <p><strong>blahh</strong></p><br>
+      <img src="images/wren.jpg" alt="Random Name" class="img-thumbnail" width="255" height="240">
+    </div>
+    <div class="col-sm-4">
+      <p><strong>blahh</strong></p><br>
+      <img src="images/mom2.jpg" alt="Random Name" class="img-thumbnail" width="255" height="240">
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br><br>
+
+
+
+
+
+<div class="container-fluid text-center">
+  <h2>SERVICES</h2><br>
+  <h4>What we offer</h4>
+  <br>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-heart"></span>
+      <h4>lOVE</h4>
+      <p>Passionately..</p>
+    </div>
+    <div class="col-sm-4">
+       <span class="glyphicon glyphicon-lock"></span>
+      <h4>BABY SAFETY</h4>
+      <p>Like our child...</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-star"></span>
+      <h4>INTENSIVE CARE</h4>
+      <p>100%..</p>
+    </div>
+  </div>
+  <br><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-ok-sign"></span>
+      <h4>QUALITY</h4>
+      <p>No doubt...</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-leaf"></span>
+      <h4>HEALTHY ENVIRONMENT</h4>
+      <p>To be in..</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-thumbs-up"></span>
+      <h4 style="color:#303030;">ENSURE HAPPINESS</h4>
+      <p>To visit us back..</p>
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<br>
+
+<div class="container-fluid bg-dark text-white">
+
+  <div class="row">
+    <div class="col-sm-5">
+      <br>
+      <br>
+      <p><i class=" glyphicon glyphicon-map-marker"></i> Bangalore, India</p>
+      <p><i class="glyphicon glyphicon-phone"></i> +91 9036123628</p>
+      <p><i class="glyphicon glyphicon-envelope"></i> aishu23628@gmail.com</p>
+      <br>
+      <br>
+    </div>
+  </div>
+</div>
+
